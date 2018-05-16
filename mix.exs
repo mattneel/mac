d595b8aec2ef1bca4b830def73e1b0394f9a5054
Matt Neel <matt.neel@vallationinc.com>
2018-05-15 21:47:29 -1000

    Fix warnings.

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule MAC.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :mac,
-     version: "0.2.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps,
-     description: "MAC-to-vendor search for Elixir.",
-     package: package]
+    [
+      app: :mac,
+      version: "0.2.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      description: "MAC-to-vendor search for Elixir.",
+      package: package()
+    ]
   end
 
   def application do
@@ -21,9 +23,11 @@ defmodule MAC.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Johanna Appel"],
-     licenses: ["WTFPL"],
-     files: ["priv", "lib", "mix.exs", "README*", "LICENSE*"],
-     links: %{"GitHub" => "https://github.com/ephe-meral/mac"}]
+    [
+      maintainers: ["Johanna Appel"],
+      licenses: ["WTFPL"],
+      files: ["priv", "lib", "mix.exs", "README*", "LICENSE*"],
+      links: %{"GitHub" => "https://github.com/ephe-meral/mac"}
+    ]
   end
 end
